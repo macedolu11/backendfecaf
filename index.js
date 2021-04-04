8e254a8094bf8118f2fbdfc5cfae92ccdb4f8a86
Lmacedox <81891044+macedolu11@users.noreply.github.com>
2021-04-04 01:24:05 -0300

    UPDATES

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+const express  = require("express")
+const path = require('path')
+const config = require('config')
+
+
+const port = process.env.PORT || config.set("server.port");
+
+const app = express();
+
+
+app.set("port", port)
+
+
+app.route('/extrato').get(
+  (req, res) => {
+    res.status(200).json(
+      {'b': 'as'}
+      )
+  }
+)
+
+
+
+app.listen(port, () =>{
+  console.log("RATO AGUADO")
+})
 console.log("Server Carapato")
