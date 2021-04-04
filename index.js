bd40363de0c6de1e17a380235a4e3549b24a20c8
Lmacedox <81891044+macedolu11@users.noreply.github.com>
2021-04-04 16:49:54 -0300

    UPDATES

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express  = require("express");
 const path = require('path');
 const config = require("config")
 const pg = require("pg")
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 const pool = new pg.Pool({
   connectionString: 'postgres://prkdhhgasrebsu:0481eb0c3ca0e5831799027422f761b66b307da385ab3fabc98f5bc32c3f946a@ec2-54-196-33-23.compute-1.amazonaws.com:5432/d8spculdfeicsn',
@@ -14,7 +19,7 @@ const pool = new pg.Pool({
 
 const port = process.env.PORT || config.get("server.port");
 
-const app = express();
+
 
 
 app.set("port", port)
@@ -39,16 +44,6 @@ const lancamentos = [
   }
 ]
 
-app.route('/extrato').get( 
-  (req, res) => {
-    let querySelectAll = "SELECT * from lancamentos"
-    pool.query(querySelectAll, (err, dbres) => {
-      console.log("Error: " + err)
-      res.status(200).json(dbres.rows)
-    });
-
-  })
-
 app.route('/reset').get(
   (req, res) => {
     let dropCreatTable = "";
@@ -67,15 +62,26 @@ app.route('/reset').get(
     });
   });
 
-
-app.route('/lancamento').get(
+app.route('/extrato').get(
   (req, res) => {
-    console.log(req.body)
-    // let querySelectAll = "INSERT INTO lancamentos(nome,sabor,tamanho,quantidade) VALUES ()"
-    // pool.query(querySelectAll, (err, dbres) => {
-    //   console.log("Error: " + err)
+    let querySelectAll = "SELECT * from lancamentos"
+    pool.query(querySelectAll, (err, dbres) => {
+      console.log("Error: " + err)
       res.status(200).json(dbres.rows)
-    // });
+    });
+
+  })
+
+
+
+app.route('/lancamento').post(
+  (req, res) => {
+    let queryInset = "INSERT INTO lancamentos (nome,sabor,tamanho,quantidade) VALUES "
+    queryInset += `('${req.body.nome}', '${req.body.sabor}', '${req.body.tamanho}', '${req.body.quantidade}');`;
+    console.log(queryInset)
+    pool.query(queryInset, (err, dbres) => {
+      res.status(200).send("OK")
+    });
     
   }
 )
