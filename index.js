f05113549a6becb5b5bc0502e42d3be69b6cf7f8
Lmacedox <81891044+macedolu11@users.noreply.github.com>
2021-04-04 01:30:17 -0300

    up

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express  = require("express")
-const path = require('path')
-const config = require('config')
+const express  = require("express");
+const path = require('path');
+const config = require("config")
 
 
 const port = process.env.PORT || config.set("server.port");
