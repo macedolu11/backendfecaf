bcbbb3f4b98511bef81675e435cce1cc7f6b3c2d
Lmacedox <62385353+Lmacedox@users.noreply.github.com>
2021-05-24 21:18:02 -0300

    All Refresh

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ const express  = require("express");
 const path = require('path');
 const config = require("config")
 const pg = require("pg")
+const jwt = require('jsonwebtoken')
 const bodyParser = require("body-parser");
+const uri = config.get("bd.uri")
+const secret = config.get("jwt.secret")
+
 
 const app = express();
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
 const pool = new pg.Pool({
-  connectionString: 'postgres://prkdhhgasrebsu:0481eb0c3ca0e5831799027422f761b66b307da385ab3fabc98f5bc32c3f946a@ec2-54-196-33-23.compute-1.amazonaws.com:5432/d8spculdfeicsn',
+  connectionString: uri,
   ssl:{
     rejectUnauthorized: false
   }
@@ -48,12 +52,21 @@ app.route('/reset').get(
   (req, res) => {
     let dropCreatTable = "";
     dropCreatTable += "DROP TABLE IF EXISTS lancamentos; ";
-    dropCreatTable += "CREATE TABLE lancamentos(";
+    dropCreatTable += "CREATE TABLE lancamentos ( ";
     dropCreatTable += "nome varchar(50), "
     dropCreatTable += "sabor varchar(150), "
     dropCreatTable += "tamanho varchar(130), "
     dropCreatTable += "quantidade varchar(10) "
     dropCreatTable += ");";
+    dropCreatTable += "DROP TABLE IF EXISTS usuarios; ";
+    dropCreatTable += "CREATE TABLE usuarios (";
+    dropCreatTable += "usuario varchar(50), "
+    dropCreatTable += "senha varchar(255), "
+    dropCreatTable += "perfil varchar(150), "
+    dropCreatTable += "nome varchar(50) "
+    dropCreatTable += ");"
+    dropCreatTable += "INSERT INTO usuarios (usuario,senha,perfil,nome) "
+    dropCreatTable += "VALUES ('admin', '123456', 'ADMIN', 'Lucas Macedo'); "
 
     pool.query(dropCreatTable, (err, dbres) => {
       console.log("Error: " + err)
@@ -85,6 +98,31 @@ app.route('/lancamento').post(
     
   }
 )
+
+
+app.route('/login').post((req,res) =>{
+  let qry = `SELECT * FROM usuarios WHERE usuario = '${req.body.usuario}'`
+  qry += ` AND senha  = '${req.body.senha}'`
+  pool.query(qry, (err, dbres) =>{
+    if(err){
+      res.status(500).send(err)
+    } else {
+      if(dbres.rowCount > 0){
+        const row = dbres.rows[0]
+        const payload = {
+          usuario: row.usuario,
+          perfil: row.perfil,
+          nome: row.nome
+        }
+        const token = jwt.sign(payload, secret)
+        const objToken = {token}
+        res.status(200).json(objToken);
+      } else {
+        res.status(401).send("Usuario ou senha invalidos")
+      }
+    }
+  })
+});
 
 
 
